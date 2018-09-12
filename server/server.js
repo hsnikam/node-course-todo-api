510f7270004d9b56b3107ef9a1d68a1d578e71e1
Harshit Nikam <hsnikam@cygnet.com>
2018-09-12 16:01:17 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var{ObjectId} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
@@ -29,6 +30,28 @@ app.get('/todos',(req,res)=>{
         res.status(400).send(e);
     })
 })
+
+app.get('/todos/:id',(req,res)=>{
+    
+     var id = req.params.id;
+
+     //idvalid id
+     if(!ObjectId.isValid(id)){
+         console.log('Id is not valid');
+         res.status(404).send();
+     }
+
+     Todo.findById(id).then((todo) => {
+        if(!todo){
+        console.log('Todo not found');
+        res.status(404).send();
+        }
+        console.log('To do by id',todo);
+        res.send(todo);
+    }).catch((e)=>console.log(e));
+     
+});
+
 app.listen(3000,() => {
     
 })
